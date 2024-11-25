45304ea70d76756e53aa009683f04b572df6b62c
GrabinaSergey <dieumortx@gmail.com>
2024-11-25 22:36:34 +0500

    Task3

---
+++ Tyuiu.GrabinaSA.Sprint5.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.GrabinaSA.Sprint5.Task2.V29.Lib;
+namespace Tyuiu.GrabinaSA.Sprint5.Task2.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool Exists = fileInfo.Exists;
+            Assert.IsTrue(Exists);
+        }
+    }
+}
