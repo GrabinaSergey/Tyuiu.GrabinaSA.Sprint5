f94f919ded06e7066ac1c51ee1b5d9558f93a85b
GrabinaSergey <dieumortx@gmail.com>
2024-11-27 00:21:39 +0500

    Task7

--- Tyuiu.GrabinaSA.Sprint5.Task7.V23.Test/DataServiceTest.cs
+++ Tyuiu.GrabinaSA.Sprint5.Task7.V23.Test/DataServiceTest.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.GrabinaSA.Sprint5.Task7.V23.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Path.Combine(@"D:/repos/Tyuiu.GrabinaSA.Sprint5/Tyuiu.GrabinaSA.Sprint5.Task5.V26/bin/Debug/net8.0/OutPutFileTask1.txt");
-            FileInfo fileInfo = new FileInfo(Path.Combine());
+            Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(Path.GetTempFileName());
             bool fileExists = fileInfo.Exists;
             Assert.IsTrue(fileExists);
         }
