2c842d8664337626edae4a5e8138403cec82d7c8
GrabinaSergey <dieumortx@gmail.com>
2024-11-27 00:03:00 +0500

    Task7

---
+++ Tyuiu.GrabinaSA.Sprint5.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.GrabinaSA.Sprint5.Task7.V23.Lib;
+namespace Tyuiu.GrabinaSA.Sprint5.Task7.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"D:\repos\Tyuiu.GrabinaSA.Sprint5\Tyuiu.GrabinaSA.Sprint5.Task5.V26\bin\Debug\net8.0\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.IsTrue(fileExists);
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint5.Task7.V23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Tyuiu.GrabinaSA.Sprint5.Task7.V23.Lib;
+namespace Tyuiu.GrabinaSA.Sprint5.Task7.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"D:\repos\Tyuiu.GrabinaSA.Sprint5\Tyuiu.GrabinaSA.Sprint5.Task5.V26\bin\Debug\net8.0\OutPutFileTask1.txt";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Строка из файла:");
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.WriteLine($"Данные находятся в файле: {path}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.LoadDataAndSave(path);
+            Console.WriteLine("Полученные данные находятся в файле:");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
